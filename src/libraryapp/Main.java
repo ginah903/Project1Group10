2f6405e9d7e623e2415fdbee24acd33d3e4f587f
ginah903 <ginah903@gmail.com>
2025-02-09 17:53:05 -0600

    Create Library class with attributes and initial values
    Update imports in Main.java for Book and Library classes

---
+++ src/libraryapp/Main.java
@@ -0,0 +1,12 @@
+package libraryapp;
+import libraryapp.book.Book;
+import libraryapp.library.Library;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
