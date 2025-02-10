2dce12cf3aedb8136324be0c5178f4b59eef3e2b
jackeveker <jackeveker@mwc-149144.dhcp.missouri.edu>
2025-02-10 17:12:38 -0600

    added addbook method to Library.java

--- src/libraryapp/library/Library.java
+++ src/libraryapp/library/Library.java
@@ -2,12 +2,28 @@ package libraryapp.Library;
 import libraryapp.book.Book;
 
 public class Library {
-	private Book[] books;
+	private java.awt.print.Book[] books;
 	private int count;
 	
 	public Library() {
 		books = new Book[5];
 		count = 0;
+	}
+	
+	
+	/**
+	 * Method that adds a book to the Book array
+	 * 
+	 * @param book The book to be added to the array
+	 */
+	public boolean addbook(Book book) {
+		if(count < books.length) {
+			books[count] = book;
+			count++;
+			return true
+		}
+		
+		return false;
 	}
 }
 
