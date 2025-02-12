d607d11a769af56fde88bf91c1ae22a862af2ab9
ginah903 <ginah903@gmail.com>
2025-02-12 15:29:49 -0600

    Update Main.java - fixed wrong merge

--- src/libraryapp/Main.java
+++ src/libraryapp/Main.java
@@ -1,16 +1,8 @@
 package libraryapp;
 import libraryapp.book.Book;
-import mu.edu.todolist.tasks.Task;
-import mu.edu.todolist.tasks.WorkTask;
-
 import libraryapp.library.Library;
-
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WorkTask workTask = new WorkTask("Pay bills", "Pay the utility bills.");
-		System.out.println("Description: " + workTask.getDescription());
 	}
-
 }
