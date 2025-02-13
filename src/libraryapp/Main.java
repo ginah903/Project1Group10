91d8ee32245df81923140fe64da6949e26cfd88f
rai8d4 <122903297+rai8d4@users.noreply.github.com>
2025-02-12 20:14:59 -0600

    edited searchByISBN method and attempting new test input

--- src/libraryapp/Main.java
+++ src/libraryapp/Main.java
@@ -45,11 +45,13 @@ public class Main {
 		}
 		
 		//searching for book by ISBN test
-		if (myLibrary.searchByISBN("938-0256278379") {
+		Book testBook = myLibrary.searchByISBN("938-0256278379");
+		System.out.println("Searching for book with ISBN: 938-0256278379");
+		if (testBook != null) {
 			System.out.println("Book found: Beloved by Toni Morrison (ISBN: 938-0256278379, $24.99");
 		}
 		else {
 			System.out.println("Could not find book with ISBN: 938-0256278379");
 		}
-	}
+		
 }
